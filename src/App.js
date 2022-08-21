02291eba4512dbd7304a99abc08a1f713f246be2
luizmarcelolm <luizmarcelolm@hotmail.com>
2022-08-21 17:55:02 -0300

    Validação do formulário

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { useState } from 'react';
 import './App.css';
 
    
-  function App() {
-
+function App() {
+    
            
     const [formValues, setFormValues] = useState({});
 
@@ -14,24 +14,64 @@ import './App.css';
     setFormValues({...formValues, [name]: value});
    
    }
-
-
     const handleSubmit = (e) =>{
+
+      const input_element_name=document.getElementById("inputName");
+      const input_element_email=document.getElementById("inputEmail");
+      
+      const validacao_input_name = () => input_element_name.value.length > 0;
+      const validacao_input_email = () => input_element_email.value.length > 0;
+
+       //tirar o vermelho do input caso tenha texto no campo nome
+       const handle_input_change_name = () => {
+        const inputIsValid_name = validacao_input_name();
+        if (inputIsValid_name) {
+            return input_element_name.classList.remove("erro");
+        }
+      };
+       input_element_name.addEventListener("change", () => handle_input_change_name());  
+
+       //tirar o vermelho do input caso tenha texto no campo email
+       const handle_input_change_email = () => {
+        const inputIsValid_email = validacao_input_email();
+        if (inputIsValid_email) {
+            return input_element_email.classList.remove("erro");
+        }
+      };
+       input_element_email.addEventListener("change", () => handle_input_change_email());  
+  
+      
+      const inputIsValid_name = validacao_input_name();
       e.preventDefault();
-      const formData = new FormData(e.target);
-      const data = Object.fromEntries(formData)
+          
+      if (!inputIsValid_name) {
+         input_element_name.classList.add("erro"); 
+         return;
+        
+      }  
+             
+      const inputIsValid_email = validacao_input_email();
+      e.preventDefault();
+          
+      if (!inputIsValid_email) {
+        return input_element_email.classList.add("erro");
+      }
 
-      console.log('***handleSubmit', data);
-    };
-
-    
-    return (
+      if (inputIsValid_name & inputIsValid_email) {
+        document.getElementById("inputName").value = "";
+        document.getElementById("inputEmail").value = "";
+        return alert("CADASTRO ENVIADO COM SUCESSO!!!");
+        
+      } 
+    }     
+      
+    return (          
              
          <form onSubmit = {handleSubmit}>
            <div className="container">
-            <h1>CADASTRO DEV</h1>
-            <input class="input" type="text" name="name" placeholder="nome" onChange={handleInputChange} value={formValues.name || ''}/>
-            <input class="input"  type="email" name="email" placeholder="email" onChange={handleInputChange} value={formValues.email || ''}/>
+            <h1>CADASTRO</h1>
+            <input class="input" type="text" name="name" placeholder="nome" id="inputName" onChange={handleInputChange} value={formValues.name || ''}/>
+            <input class="input"  type="email" name="email" placeholder="email" id="inputEmail" onChange={handleInputChange} value={formValues.email || ''}/>
 
             <div className="radios">
               <p>Sexo</p>
@@ -48,13 +88,12 @@ import './App.css';
               <option value="outros">Outros</option>
             </select> 
            
-            <button type="submit" onclick="validate">Enviar</button>
+            <button type="submit"> Enviar</button>
             </div> 
          </form>
       
     );
 }
-
 
 export default App;
 
